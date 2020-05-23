d94d16ae708ccbbbd16820535f3d0dec98437081
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-05-23 17:56:08 -0300

    Looping through PyPI plugins URLs

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let pypi_root = String::from("https://pypi.org/simple/");
+    let pulp_plugins = [
+        "galaxy-ng",
+        "pulp-ansible",
+        "pulp-certguard",
+        "pulp-container",
+        "pulp-cookbook",
+        "pulp-deb",
+        "pulp-file",
+        "pulp-gem",
+        "pulp-maven",
+        "pulp-npm",
+        "pulp-python",
+        "pulp-rpm",
+    ];
+
+    for plugin in pulp_plugins.iter() {
+        let res = reqwest::get(&format!("{}{}", pypi_root, plugin.to_string())).await?;
+
+        println!("Status: {}", res.status());
+
+        let body = res.text().await?;
+
+        println!("Body:\n\n{}", body);
+    }
+
+    Ok(())
+}
